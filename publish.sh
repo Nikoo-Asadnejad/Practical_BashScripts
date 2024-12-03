9e98b5ddf26ee1a05bda4fb61ef4bdbe7bac9657
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-12-03 22:11:11 +0330

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 ZIP_FILE="publish.zip"
-SERVER_USER="developer"
-SERVER_ADDRESS="192.168.1.10"
-SERVER_PORT="8090"
+SERVER_USER="your_username"
+SERVER_ADDRESS="your_server_address"
+REMOTE_PORT=22  
 REMOTE_PATH="/path/on/server"
+
 
 zip -r $ZIP_FILE . -x "appsettings.*.json" "web.config"
 
 if [ $? -eq 0 ]; then
   echo "Files successfully zipped as $ZIP_FILE."
 
-
-  scp -P $SERVER_PORT $ZIP_FILE $SERVER_USER@$SERVER_ADDRESS:$REMOTE_PATH
+  scp -P $REMOTE_PORT $ZIP_FILE $SERVER_USER@$SERVER_ADDRESS:$REMOTE_PATH
 
   if [ $? -eq 0 ]; then
     echo "File successfully copied to $SERVER_USER@$SERVER_ADDRESS:$REMOTE_PATH."
+
+    ssh -p $REMOTE_PORT $SERVER_USER@$SERVER_ADDRESS "cd $REMOTE_PATH && unzip -o $ZIP_FILE && echo 'File unzipped successfully on the server.'"
+
+    if [ $? -eq 0 ]; then
+      echo "File unzipped successfully on the server."
+    else
+      echo "Failed to unzip the file on the server."
+    fi
   else
     echo "Failed to copy the file to the server."
   fi
